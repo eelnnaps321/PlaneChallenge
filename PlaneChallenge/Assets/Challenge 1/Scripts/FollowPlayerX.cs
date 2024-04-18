f4533c6fba94891482a5e94a5fc38580be49d0ca
leespann <spannl@roverkids.org>
2024-04-18 13:59:11 -0400

    plane challenge complete

--- PlaneChallenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ PlaneChallenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(38, 2, 2);
+    private Vector3 offset = new Vector3(38, 2, 2) ;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
